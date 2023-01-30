92e042f60c929589e18bc1d9cbedc5580d6710cf
Finn De Bie <S00238716@atu.ie>
2023-01-30 09:45:52 +0000

    Question 1 finished

--- Q1/Program.cs
+++ Q1/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace Q1
+﻿/*
+ * Name : Finn De Bie   
+ * Date : 30 - 1 - 23
+ * Desc : odd numbers between 2 values added
+ */
+namespace Q1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"-- Enter range of integers --");
+            Console.Write($"Enter min : ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.Write($"Enter max : ");
+            int n2 = int.Parse(Console.ReadLine());
+            // user input
+            
+            int result = Sum(n1, n2);
+
+            Console.WriteLine($"\nResult : {result}");
+        }
+
+        static int Sum(int n1, int n2)
+        {
+            int total = 0;
+
+            for (int i = n1; i <= n2; i++)
+            {
+         
+                if (i % 2 != 0)
+                {
+                    total += i;
+                }
+                // adds the value of odd numbers to the total
+            }
+            // loops through all the numbers from min to max 
+
+            return total;
         }
     }
 }
