c0a6efbc1b01c66d1e44b2aa99c8bfc864bd2cc2
Finn De Bie <S00238716@atu.ie>
2023-01-30 10:16:05 +0000

    Question 3 finished

---
+++ Q3/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Name : Finn De Bie   
+ * Date : 30 - 1 - 23
+ * Desc : age checker
+ */
+
+namespace Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age : ");
+            int userAge = int.Parse(Console.ReadLine());
+            // user input
+
+            Console.WriteLine("");
+
+            bool result = AgeCheck(userAge);
+            // result brought to main 
+
+            Console.WriteLine($"Can access site? : {result}");
+            // display
+        }
+
+        static bool AgeCheck(int userAge)
+        {
+            if (userAge >= 18 && userAge <= 21)
+            {
+                return true;
+            }
+            else return false;
+            // checks if condition is true 
+        }
+    }
+}
